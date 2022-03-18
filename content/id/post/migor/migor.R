0af6c3b80fb5500b4670bc556ad94783b4ef708e
imedkrisna <imedkrisna@gmail.com>
2022-03-18 10:38:33 +0700

    tambah highlight kalimantan timur

--- content/id/post/migor/migor.R
+++ content/id/post/migor/migor.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 library(gghighlight)
-migor<-readxl::read_excel('migor.xlsx') %>%
+migor<-readxl::read_excel("C:/github/krisna/content/id/post/migor/migor.xlsx") %>%
   mutate(Bulan = parse_date(Bulan, "%m/%Y")) %>%
   pivot_longer(!Bulan,names_to = "provinsi",values_to = "harga")
 ggplot(data=migor,aes(Bulan,harga,color=provinsi))+geom_line()+
-  gghighlight(provinsi%in%c("PAPUA BARAT","DKI JAKARTA","KEPULAUAN RIAU"))+
+  gghighlight(provinsi%in%c("PAPUA BARAT","DKI JAKARTA","KEPULAUAN RIAU","KALIMANTAN TIMUR"))+
   theme_classic()+
   labs(
     x="Bulan",
